86eda3078956725590540d79cb0d89edaf732c1e
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-11 02:56:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,6 +12,8 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma
-  
+def speak_to_grandma(x)
+  if x.upcase
+    puts ""
 end
+speak_to_grandma
